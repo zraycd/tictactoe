c7edcbe7932875e574946759ea1bbe00758dca95
zraycd <vainiusvjg@gmail.com>
2024-03-25 12:08:35 +0200

    cleanup & finish place logic

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function createGameboard(row) {
-    const row1 = [' ', ' ', ' ']
-    const row2 = [' ', ' ', ' ']
-    const row3 = [' ', ' ', ' ']
+function createGameboard() {
+    let row1 = [' ', ' ', ' ']
+    let row2 = [' ', ' ', ' ']
+    let row3 = [' ', ' ', ' ']
 
-    const place = (column, marker) => {
-        if (row === 0) {
-            this.row1[column - 1] = `${marker}`
-        }
+    const gameboard = [row1, row2, row3]
+
+    const place = (row, column, marker) => {
+        gameboard[row - 1][column - 1] = marker
     }
-    return { row1, row2, row3, place }
+    return { gameboard, place }
 }
 function createPlayer (name, mark) {
 
@@ -21,26 +21,14 @@ function createPlayer (name, mark) {
 
     return { playerName, playerMark, getCurrent, move }
 }
-function putMarker (row, column, marker) {
-    let row1 = createGameboard(0)
-    let row2 = createGameboard(1)
-    let row3 = createGameboard(2)
-
-    
-    if (row === '0') {
-        row1.place(column, marker)
-    } else if (row === '1') {
-        row2.place(column, marker)
-    } else if (row === '2') {
-        row3.place(column, marker)
-    }
-}
 
 function playGame() {
+    let gameboard = createGameboard()
 
     const player1 = createPlayer(prompt('Player1 name:'), prompt('Player1 mark:'))
     const player2 = createPlayer(prompt('Player2 name:'), prompt('Player2 mark:'))
 
-    putMarker(prompt(`${player1.playerName}, what row?`), prompt(`${player1.playerName}, what column?`), player1.playerMark)
+    gameboard.place(prompt(`${player1.playerName}, what row?`), prompt(`${player1.playerName}, what column?`), player1.playerMark)
+    console.log(gameboard)
 }
 playGame()
