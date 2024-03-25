cd579a61cd48adf6dec3e8a85a9f60379460494f
zraycd <vainiusvjg@gmail.com>
2024-03-25 11:50:36 +0200

    add place logic

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function createGameboard(row) {
     const row2 = [' ', ' ', ' ']
     const row3 = [' ', ' ', ' ']
 
-    const changeRow = (column) => {}
-    return { row1, row2, row3 }
+    const place = (column, marker) => {
+        if (row === 0) {
+            this.row1[column - 1] = `${marker}`
+        }
+    }
+    return { row1, row2, row3, place }
 }
 function createPlayer (name, mark) {
 
@@ -17,10 +21,19 @@ function createPlayer (name, mark) {
 
     return { playerName, playerMark, getCurrent, move }
 }
-function putMarker (row, column) {
-    let gameboard = createGameboard()
-    let line = row
-    gameboard.line
+function putMarker (row, column, marker) {
+    let row1 = createGameboard(0)
+    let row2 = createGameboard(1)
+    let row3 = createGameboard(2)
+
+    
+    if (row === '0') {
+        row1.place(column, marker)
+    } else if (row === '1') {
+        row2.place(column, marker)
+    } else if (row === '2') {
+        row3.place(column, marker)
+    }
 }
 
 function playGame() {
@@ -28,10 +41,6 @@ function playGame() {
     const player1 = createPlayer(prompt('Player1 name:'), prompt('Player1 mark:'))
     const player2 = createPlayer(prompt('Player2 name:'), prompt('Player2 mark:'))
 
-    for (let i = 0;i < 5;i++) {
-        let putMarker = prompt(`${player1.playerName}, what row do you want to go?`)
-    }
+    putMarker(prompt(`${player1.playerName}, what row?`), prompt(`${player1.playerName}, what column?`), player1.playerMark)
 }
 playGame()
-
-
