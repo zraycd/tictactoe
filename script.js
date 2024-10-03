523a5338e221109a5fb27ca82aef98d71578497d
zray <strayzray@gmail.com>
2024-10-03 19:43:37 +0300

    add checkForWin func for normal tictactoe

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-class Gameboard {
+class Gameb {
   constructor(type) {
     this.type = type;
     //prettier-ignore
-    this.board = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => null));
+    this.b = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => null));
     if (this.type !== "normal") {
       //prettier-ignore
-      this.board = this.board.map(row => row.map(() => new Gameboard("normal").board));
+      this.b = this.b.map(row => row.map(() => new Gameb("normal").b));
     }
     this.moveCount = 0;
     this.lastMark = null;
@@ -13,9 +13,9 @@ class Gameboard {
 
   getFocus() {
     if (this.lastMark === null) {
-      return this.board;
+      return this.b;
     } else {
-      return this.board[this.lastMark.row][this.lastMark.col];
+      return this.b[this.lastMark.row][this.lastMark.col];
     }
   }
 
@@ -38,22 +38,51 @@ class Gameboard {
     this.moveCount++;
   }
 
-  resetBoard() {
+  resetb() {
     this.moveCount = 0;
     this.type === "normal"
-      ? this.board.forEach((row, rowIndex) => {
+      ? this.b.forEach((row, rowIndex) => {
           row.forEach((col, colIndex) => {
-            this.board[rowIndex][colIndex] = null;
+            this.b[rowIndex][colIndex] = null;
           });
         })
-      : this.board.forEach((row) => {
-          row.forEach((innerBoard) => {
-            innerBoard.forEach((innerRow, innerRowIndex) => {
+      : this.b.forEach((row) => {
+          row.forEach((innerb) => {
+            innerb.forEach((innerRow, innerRowIndex) => {
               innerRow.forEach((cell, cellIndex) => {
-                innerBoard[innerRowIndex][cellIndex] = null;
+                innerb[innerRowIndex][cellIndex] = null;
               });
             });
           });
         });
+  }
+
+  checkForWin(b) {
+    // prettier-ignore
+    const winningCombinations = [
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ];
+
+    for (let combination of winningCombinations) {
+      const [x, y, z] = combination;
+      if (
+        b[x[0]][x[1]] !== null &&
+        b[x[0]][x[1]] === b[y[0]][y[1]] &&
+        b[x[0]][x[1]] === b[z[0]][z[1]]
+      ) {
+        return combination;
+      }
+    }
+
+    return null;
   }
 }
