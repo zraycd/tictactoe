05516e1cfe7e4cf4c546358423846ec95daa90a9
zray <strayzray@gmail.com>
2024-10-08 20:53:03 +0300

    progress to UI functionality, bugs to be fixed

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ class Gameboard {
   constructor(type) {
     this.type = type;
     //prettier-ignore
-    this.board = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => null));
+    this.board = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => ""));
     if (this.type !== "normal") {
       //prettier-ignore
       this.board = this.board.map(row => row.map(() => new Gameboard("normal").board));
     }
     this.moveCount = 0;
     this.lastMark = null;
+    this.winner = null;
   }
 
   getFocus() {
@@ -20,10 +21,11 @@ class Gameboard {
   }
 
   placeMarker(row, col) {
+    // try {
     if (this.type === "normal") {
       this.lastMark = null;
 
-      if (this.getFocus()[row][col] !== null) {
+      if (this.getFocus()[row][col] !== "") {
         return;
       }
 
@@ -32,14 +34,15 @@ class Gameboard {
         : (this.getFocus()[row][col] = "O");
     } else if (this.moveCount === 0) {
       this.lastMark = { row: row, col: col };
-    } else if (this.getFocus()[row][col] !== null) {
+    } else if (this.getFocus()[row][col] !== "") {
       return;
     } else {
       this.moveCount % 2 === 1
         ? (this.getFocus()[row][col] = "X")
         : (this.getFocus()[row][col] = "O");
     }
-
+    this.hideBoard();
+    this.displayBoard();
     this.moveCount++;
     let winCheckUlt;
     try {
@@ -50,23 +53,32 @@ class Gameboard {
     }
 
     if (winCheckUlt !== null) {
+      this.hideBoard();
+      this.displayBoard();
       let i = this.board[this.lastMark.row][this.lastMark.col];
       //prettier-ignore
       this.board[this.lastMark.row][this.lastMark.col] = i[winCheckUlt[0][0]][winCheckUlt[0][1]];
     }
     if (this.checkForWin(this.board) !== null) {
       let i = this.checkForWin(this.board);
+      this.winner = i;
+      this.hideBoard();
+      this.displayBoard();
       this.board = this.board[i[0][0]][i[0][1]];
-      return `${this.board} wins`;
+      return;
     }
-
     this.lastMark = { row: row, col: col };
+    console.log("change");
+
+    // } catch {
+    //   return;
+    // }
   }
 
   resetBoard() {
     this.moveCount = 0;
     //prettier-ignore
-    this.board = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => null))
+    this.board = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => ""))
     if (this.type !== "normal") {
       //prettier-ignore
       this.board = this.board.map(row => row.map(() => new Gameboard("normal").board));
@@ -91,7 +103,7 @@ class Gameboard {
     for (let combination of winningCombinations) {
       const [x, y, z] = combination;
       if (
-        b[x[0]][x[1]] !== null &&
+        b[x[0]][x[1]] !== "" &&
         b[x[0]][x[1]] === b[y[0]][y[1]] &&
         b[x[0]][x[1]] === b[z[0]][z[1]]
       ) {
@@ -102,24 +114,37 @@ class Gameboard {
     return null;
   }
   displayBoard(mainContainer = document.querySelector(".mainContainer")) {
+    //prettier-ignore
+    const coordinates = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]];
+    let j = 0;
+
     if (this.type === "normal") {
       this.board.forEach((mainRow) => {
         mainRow.forEach((mainCol) => {
           let cell = document.createElement("div");
           cell.classList.add("cell");
+          cell.dataset.coordinates = JSON.stringify(coordinates[j]);
+          cell.textContent = mainCol;
           mainContainer.appendChild(cell);
+          j++;
         });
       });
     } else {
+      j = 0;
+
       this.board.forEach((mainRow) => {
         mainRow.forEach((mainCol) => {
           let container = document.createElement("div");
           container.classList.add("container");
+          container.dataset.coordinates = JSON.stringify(coordinates[j]);
           mainContainer.appendChild(container);
+          j++;
         });
       });
+
       document.querySelectorAll(".container").forEach((container) => {
         let i = new Gameboard("normal");
+        i.board = this.board;
         i.displayBoard(container);
       });
     }
@@ -139,6 +164,8 @@ const ultimateGame = new Gameboard("ultimate");
 let current;
 
 normalGame.displayBoard();
+current = normalGame;
+
 normalBtn.addEventListener("click", () => {
   current = normalGame;
   ultimateGame.hideBoard();
@@ -153,4 +180,25 @@ ultimateBtn.addEventListener("click", () => {
 
 resetBtn.addEventListener("click", () => {
   current.resetBoard();
+  current.hideBoard();
+  current.displayBoard();
 });
+
+document.querySelector(".mainContainer").addEventListener("click", (event) => {
+  handleClick(event.target);
+  console.log(event.target);
+});
+
+function handleClick(target) {
+  let coordinates;
+  if (current.type === "normal" || current.moveCount > 0) {
+    coordinates = JSON.parse(target.getAttribute("data-coordinates"));
+  } else {
+    coordinates = JSON.parse(
+      target.parentElement.getAttribute("data-coordinates")
+    );
+  }
+  current.placeMarker(coordinates[0], coordinates[1]);
+  current.hideBoard();
+  current.displayBoard();
+}
