a513275eef6257d14421d2fc1e30afa19540ed14
zray <strayzray@gmail.com>
2025-03-30 00:27:40 +0200

    newdeploy try n2

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Clears old files from dist
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "",
       template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
-  mode: "development",
+  mode: "production",
 };
