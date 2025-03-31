b3385df1562e69cf5d8fc6a79a86d91db97a2dfc
zray <strayzray@gmail.com>
2025-03-31 17:10:34 +0300

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-TIC TAC TOE game with 2 options:
-
-- Normal: play on a 3x3 board
-- Ultimate: play on a 3x3 board where each cell contains a normal TTT board
--- src/gameboard.js
+++
@@ -1,9 +0,0 @@
-import gameboardManipulation from "./gameboardBoardManipulation";
-
-class Gameboard extends gameboardManipulation {
-  constructor(type) {
-    super(type);
-  }
-}
-
-export default Gameboard;
